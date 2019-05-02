abbd4864b68d7fd8fa92feb7757dcf7e409da3b2
Patrick McNamee <pjmcnamee@comcast.net>
2019-05-02 11:47:11 -0600

    'push'

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      slideState: false
+    }
+  }
+
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+        <header className="header">
+          <div className="header-top">
+            <h2 className="title">Start Bootstrap</h2>
+            <div className="nav-holder">
+              <span>SERVICES</span>
+              <span>PORTFOLIO</span>
+              <span>ABOUT</span>
+              <span>TEAM</span>
+              <span>CONTACT</span>
+            </div>
+            <div onClick={() => this.setState({ slideState : !this.state.slideState})} className="ham">|||menu</div>
+            <div className={this.state.slideState ?'drop-down slide' :'drop-down'}>
+              <span>SERVICES</span>
+              <span>PORTFOLIO</span>
+              <span>ABOUT</span>
+              <span>TEAM</span>
+              <span>CONTACT</span>
+            </div>
+          </div>
+
+          <div className="header-bottom">
+            <div className="welcome">
+              <h1>Welcome to Our Studio!</h1>
+            </div>
+            <div className="meet">
+              <h1>IT'S NICE TO MEET YOU</h1>
+            </div>
+            <button className="tell">TELL ME MORE</button>
+          </div>
         </header>
       </div>
     );
